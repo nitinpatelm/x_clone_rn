12f04575f91ee95d4d7ee920aa092e6985f9a9f5
Nitin Patel M <nitinpatelm@Nitins-MacBook-Air.local>
2025-07-10 15:29:26 +0530

    db-config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(8001, (err) => {
-  console.log("server running on port 8001");
+connectDB();
+
+app.get("/", (req, res) => {
+  console.log("running");
+  res.send("hello from server");
+});
+
+app.listen(ENV.PORT, (err) => {
+  console.log("server running on port: ", ENV.PORT);
 });
