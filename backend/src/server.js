040f63e4243520f9f0ef80dd9f9fa4b2207659a4
Nitin Patel M <nitinpatelm@Nitins-MacBook-Air.local>
2025-07-14 14:15:56 +0530

    user routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,29 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
-
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 const app = express();
-
-connectDB();
-
+import userRoutes from "./routes/user.routes.js";
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
 app.get("/", (req, res) => {
   console.log("running");
   res.send("hello from server");
 });
+app.use("/api/users", userRoutes);
+const startServer = async () => {
+  try {
+    await connectDB();
 
-app.listen(ENV.PORT, (err) => {
-  console.log("server running on port: ", ENV.PORT);
-});
+    app.listen(ENV.PORT, (err) => {
+      console.log("server running on port: ", ENV.PORT);
+    });
+  } catch (error) {
+    console.log("Failed to start server", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
